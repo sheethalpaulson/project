b1c4d36fbb3d383fca7963fff7196e2151f4c4e8
sheethalpaulson <119370359+sheethalpaulson@users.noreply.github.com>
2023-03-11 11:51:10 +0800

    check

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+var mongoose = require('mongoose');
 const app = require("./app");
 const cloudinary = require("cloudinary");
-const connectDatabase = require("./config/database");
+//const connectDatabase = require("./config/database");
+const cors = require("cors");
 
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -15,15 +17,24 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 // Connecting to database
-connectDatabase();
+mongoose.connect(`mongodb+srv://Sheethal:sheethal@cluster0.qkgnpli.mongodb.net/projectDB?retryWrites=true&w=majority`,
+{
+  useNewUrlParser: true, 
 
+useUnifiedTopology: true
+
+
+})
+.then((data) => {
+  console.log(`Mongodb connected with server `);
+});
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const server = app.listen(process.env.PORT, () => {
+const servnpmer = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
